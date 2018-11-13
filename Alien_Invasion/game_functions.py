f05e2af296934656b8cd1114d77d0d2feb31e08a
liqingpeng <liqingpeng@iwubida.com>
2018-11-13 18:21:44 +0800

    增加多行飞船

--- Alien_Invasion/game_functions.py
+++ Alien_Invasion/game_functions.py
@@ -58,7 +58,7 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
-
+'''
 def create_fleet(ai_settings, screen,aliens):
     alien = Alien(ai_settings, screen)
     alien_width = alien.rect.width
@@ -70,3 +70,28 @@ def create_fleet(ai_settings, screen,aliens):
         alien.x = alien_width + 2 *alien_width * alien_number
         alien.rect.x = alien.x
         aliens.add(alien)
+'''
+def get_number_aliens_x(ai_settings, alien_width):
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_alien_x = int(available_space_x / (2 * alien_width))
+    return number_alien_x
+
+def create_alien(ai_settings, screen, aliens, alien_number):
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    aliens.add(alien)
+
+def create_fleet(ai_settings, screen, aliens):
+    alien = Alien(ai_settings, screen)
+    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+
+    for alien_number in range(number_aliens_x):
+        create_alien(ai_settings, screen, aliens, alien_number)
+
+
+def get_number_rows(ai_settings, ship_height, alien_height):
+    available_space_y =(ai_settings.screen_height - (3 * alien_height) - ship_height)
+    number_rows = int(available_space_y / (2* alien_height))
+    return number_rows
